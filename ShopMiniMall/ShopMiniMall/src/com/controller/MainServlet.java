4cbe86ce5c30ec17fa1f3ec7b02656c59e10cb6b
ParkSeolah <paouoeq@naver.com>
2023-08-23 11:44:39 +0900

    ShopMiniMall : 메인 목록 카테고리별 이미지 출력 2023-08-23

--- ShopMiniMall/ShopMiniMall/src/com/controller/MainServlet.java
+++ ShopMiniMall/ShopMiniMall/src/com/controller/MainServlet.java
@@ -18,7 +18,11 @@ public class MainServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		// 상품목록
-		String gCategory="top"; // 처음 메인화면에 보여줄 카테고리 설정
+		String gCategory = request.getParameter("gCategory"); // 카테고리 누르면 돌아오는 값을 받음
+		if(gCategory == null) { // 메인을 처음 요청하면 gCategory가 null 값
+			gCategory = "top"; // 처음 메인화면에 보여줄 카테고리 설정
+			
+		}
 		
 		GoodsService service = new GoodsServiceImpl();
 		List<GoodsDTO> list = service.goodsList(gCategory);
