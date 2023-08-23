a97cd42ffea7de76a3789b1b5bd86401a0e65c07
ParkSeolah <paouoeq@naver.com>
2023-08-23 10:00:20 +0900

    ShopMiniMall : 회원가입 구현 2023-08-22

--- ShopMiniMall/ShopMiniMall/src/com/controller/MemberUIServlet.java
+++ ShopMiniMall/ShopMiniMall/src/com/controller/MemberUIServlet.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @WebServlet("/MemberUIServlet")
 public class MemberUIServlet extends HttpServlet {
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		// 요청위임-포워드
+
+		//포워드
 		request.getRequestDispatcher("memberForm.jsp").forward(request, response);
 	}
 
